2fbd0a33974ee92c81f6c1a3986e03da60c775c6
Michael Hu <43020369+GuessHuIAm@users.noreply.github.com>
2024-02-02 15:40:54 -0500

    initialize project

---
+++ scheduler/src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+    import Calendar from '@event-calendar/core';
+    import TimeGrid from '@event-calendar/time-grid';
+    import Interaction from '@event-calendar/interaction';
+
+    let calendar;
+    let plugins = [TimeGrid, Interaction];
+    let options = {
+        view: 'timeGridWeek',
+        initialView: 'timeGridWeek',
+        selectable: true,
+        select: handleDateSelect,
+    };
+
+    function handleDateSelect(info) {
+        calendar.unselect();
+        
+        calendar.addEvent({
+            id: createEventId(),
+            start: info.startStr,
+            end: info.endStr,
+            allDay: info.allDay
+        });
+    }
+
+    function createEventId() {
+        return String(Date.now());
+    }
+</script>
+
+<Calendar bind:this={calendar} {plugins} {options} />
