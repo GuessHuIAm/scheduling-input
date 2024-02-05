d98a0d31cb8f61f79263dfdba36ef5ec58a7b34b
Olafade Omole <olafadeomole@college.harvard.edu>
2024-02-05 03:04:43 -0500

    messing wiht icons and editing

--- scheduler/src/routes/+page.svelte
+++ scheduler/src/routes/+page.svelte
@@ -3,6 +3,7 @@
     import TimeGrid from "@event-calendar/time-grid";
     import Interaction from "@event-calendar/interaction";
     import "@event-calendar/core/index.css";
+    import CalendarCheck from 'svelte-icons/fa/FaCalendarCheck.svelte';
 
     let calendar;
     let plugins = [TimeGrid, Interaction];
@@ -15,17 +16,59 @@
         select: (info) => handleEvent(info, true),
         eventDrop: (info) => handleEvent(info, false),
         eventResize: (info) => handleEvent(info, false),
+
+        /* TOP
+
+        eventContent: (info) => { 
+        return {html: '<div class="cal-container"><div class="cal-header"><div>' 
+            + info.timeText + '</div><div>X</div></div><div class="cal-body">BODY</div></div>'}; 
+        }, 
+        
+        eventClick: (info) => { 
+        if ((info.jsEvent.layerX > info.el.offsetWidth - 20) && (info.jsEvent.layerY < 20)) { 
+            calendar.removeEventById(info.event.id); 
+            setTimeout(() => { calendar.saveCalendarEvents(); }, 1000); }},
+        
+         */
+
+        eventContent: (info) => {
+          return info.event.display === 'auto'
+            ? {html: '<div>' + info.timeText + '</div>' +
+              '<button id="delete-button"> Delete </button>' +
+              // '<div class="icon"> <CalendarCheck /> </div>' + //doesn't show up here, try something else?
+              '<div>' + info.event.title + '</div>'}
+            : '';
+        },
+  
+        eventClick: (info) => {
+          if (info.event.display === 'auto') {
+            let btn = info.el.querySelector('button');
+            if (info.jsEvent.target === btn) {
+              calendar.removeEventById(info.event.id);
+            }
+            if (info.event.extendedProps.type === 'comment') {
+                //editComment(info.event);
+            }
+          }
+        },
+
+
+
+
     };
 
-    let defaultCommentOptions = [
-        "Preferable",
-        "Not Preferable",
-        "Other",
+    let defaultCommentOptions = [ //make calendar icon the deafault for normal events?
+        "Preferable", // thumbs up?
+        "Not Preferable", // thumbs down? or something more nuetral?
+        "Something Immediately Before", // using << icon?
+        "Something Immediately After", // using >> icon?
+        "Other", // unsure
     ]
 
     let commentStart, commentEnd, previewEventId;
     let commentText = defaultCommentOptions[0];
     let popupVisible = false;
+    let editPopupVisible = false;
     function handleEvent(info, isNewEvent) {
         let event, selectionStart, selectionEnd;
         const events = calendar.getEvents();
@@ -117,8 +160,6 @@
 
             /* COMMENT MERGING */ 
 
-
-
             calendar.updateEvent(event);
         }
     }
@@ -128,6 +169,12 @@
         calendar.removeEventById(previewEventId);
     }
 
+    /*
+    function hideEditPopup() {
+        editPopupVisible = false;
+    }
+    */
+
     function addNewComment() {
         const input = document.querySelector("#comment-other");
         let newCommentText = (commentText == "Other" && input.value != "") ? input.value : commentText;
@@ -135,7 +182,18 @@
         calendar.addEvent(event);
         hidePopup();
     }
-
+/*
+    function editComment(editEvent) {
+        console.log('the editcomment function actvivated');
+        editPopupVisible = true;
+        console.log('Is the popup visible?: ' + editPopupVisible);
+        const input = document.querySelector("#comment-other");
+        let newCommentText = (commentText == "Other" && input.value != "") ? input.value : commentText;
+        editEvent.title = newCommentText;
+        calendar.updateEvent(editEvent);
+        //hideEditPopup();
+    }
+*/
     function changeCommentOptions() {
         const options = document.querySelector("#comment-options");
         commentText = options.value;
@@ -214,6 +272,8 @@
     function toggleOtherVisibility() {
         otherVisibility = !otherVisibility;
     }
+
+
 </script>
 
 <div style="--color-preview: {colorPreview}">
@@ -251,6 +311,22 @@
             </div>
         </div>
     </div>
-    <div class="popup-background" style={popupVisible ? "" : "display: none;"}></div>
+    <!--<div class="popup" style={editPopupVisible ? "" : "display: none;"}>
+        <div class="popup-content">
+            Edit your comment for this time slot:
+            <select id="comment-options" on:change={changeCommentOptions}>
+                {#each defaultCommentOptions as option}
+                    <option value={option}>{option}</option>
+                {/each}
+            </select>
+            <input type="text" id="comment-other" style={commentText === "Other" ? "" : "display: none;"} />
+            <div class="popup-buttons-container">
+                <button class="popup-button" on:click={hideEditPopup}>Cancel</button>
+                <button class="popup-button" on:click={hideEditPopup}>Edit</button>
+            </div>
+        </div>
+    </div> -->
+    <div class="popup-background" style={(popupVisible && editPopupVisible) ? "" : "display: none;"}></div>
     <Calendar bind:this={calendar} {plugins} {options} />
 </div>
+
